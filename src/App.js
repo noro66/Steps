e1d2c672f48b5280da3f0f0d0a9b6e7c74e44c2e
Nouaamane <norou6@gmail.com>
2024-10-18 15:55:24 +0100

    edit  stepe div rendring

--- src/App.js
+++ src/App.js
@@ -25,9 +25,9 @@ const [step, setStep] = useState(1);
   return (
     <div className="steps">
         <div className="numbers">
-            <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-            <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-            <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+            <div className={step >= 1 ? 'active' : ''}>1</div>
+            <div className={step >= 2 ? 'active' : ''}>2</div>
+            <div className={step >= 3 ? 'active' : ''}>3</div>
          </div>
         <p className="message">Step {step}: {messages[step -1]} </p>
         <div className="buttons">
